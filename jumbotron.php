ac551c5e870f03bfe3f62854f06e61045abf5ee5
George Mamadashvili <georgemamadashvili@gmail.com>
2020-07-29 11:05:46 +0400

    Prepare minor release

--- jumbotron.php
+++ jumbotron.php
@@ -1,13 +1,15 @@
 <?php
 /**
- * Plugin Name: Jumbotron
- * Plugin URI:  https://sortabrilliant.com/jumbotron/
- * Description: With Jumbotron it's never been easier to display a gallery of videos.
- * Author:      sorta brilliant
- * Author URI:  https://sortabrilliant.com/
- * Version:     1.1.2
- * License:     GPL-2.0-or-later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Plugin Name:       Jumbotron
+ * Plugin URI:        https://sortabrilliant.com/jumbotron/
+ * Description:       With Jumbotron it's never been easier to display a gallery of videos.
+ * Version:           1.1.3
+ * Requires at least: 5.0
+ * Requires PHP:      5.6
+ * Author:            sorta brilliant
+ * Author URI:        https://sortabrilliant.com/
+ * License:           GPL-2.0-or-later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  *
  * @package Jumbotron
  */
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: sortabrilliant, mamaduka
 Tags: video, gallery, block
 Requires at least: 5.0
-Tested up to: 5.4
+Tested up to: 5.5
 Requires PHP: 5.6
-Stable tag: 1.1.2
+Stable tag: 1.1.3
 License: GPL-2.0-or-later
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 
@@ -24,6 +24,10 @@ This plugin is brought to you by [sorta brilliant](https://sortabrilliant.com/) 
 2. That's it
 
 == Changelog ==
+
+= 1.1.3 =
+* Minor style fixes.
+* Bump "Tested up to" WordPress version.
 
 = 1.1.2 =
 * Adds block description.
